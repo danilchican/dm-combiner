2d61cc062b46e33f4401f12c294c67bdae994ea1
Shakun Roman <roman.shakun@backend.expert>
2018-09-16 22:28:38 +0300

    [ADD] get_csv_from_url

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import scale
+import os
+import io
+import requests
 
 
 class DataHandler:
@@ -16,16 +17,25 @@ class DataHandler:
         return data
 
     @staticmethod
-    def read_data_json(path):
-        data = pd.read_json(path)
+    def read_data_excel(path):
+        data = pd.read_excel(path)
         return data
 
     @staticmethod
-    def read_data_excel(path):
-        data = pd.read_excel(path)
+    def get_csv_from_url(url):
+        content = requests.get(url).content
+        data = pd.read_csv(io.StringIO(content.decode('utf-8')))
         return data
 
     @staticmethod
     def convert_column_names_to_numbers(data):
         data.rename(columns={x: y for x, y in zip(data.columns, range(0, len(data.columns)))}, inplace=True)
         return data
+
+
+if __name__ == '__main__':
+    data = DataHandler.read_data_csv('../data/telecom_churn.csv')
+    print(data, end='\n' * 2 + '-' * 60 + '\n' * 2)
+    data = DataHandler.read_data_html('http://htmlbook.ru/html/table')
+    print(data, end='\n' * 2 + '-' * 60 + '\n' * 2)
+    print(DataHandler.get_csv_from_url(url="https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"))
