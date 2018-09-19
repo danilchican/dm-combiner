894871b4fc4f25f612ce239c87f75bc580ec6714
danilchican <danilchican@mail.ru>
2018-09-19 14:32:08 +0300

    Migration fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
+    private const DEFAULT_STRING_LENGTH = 191;
+
     /**
      * Bootstrap any application services.
      *
@@ -13,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(self::DEFAULT_STRING_LENGTH);
     }
 
     /**
