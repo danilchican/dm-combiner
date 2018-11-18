1c6c4999a6086387dc58bfd339e2160d919a7b5d
danilchican <danilchican@mail.ru>
2018-11-18 16:59:23 +0300

    Created models and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,10 @@
 
 namespace App\Models;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     use Notifiable;
 
@@ -16,7 +15,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'role_id', 'email', 'password',
     ];
 
     /**
@@ -29,22 +28,121 @@ class User extends Authenticatable implements JWTSubject
     ];
 
     /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
+     * Get id of the user.
      *
-     * @return mixed
+     * @return integer
      */
-    public function getJWTIdentifier()
+    public function getId(): int
     {
-        return $this->getKey();
+        return $this->id;
     }
 
     /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
+     * Get name of the user.
      *
-     * @return array
+     * @return string
      */
-    public function getJWTCustomClaims(): array
+    public function getName()
     {
-        return [];
+        return $this->name;
+    }
+
+    /**
+     * Set name for user.
+     *
+     * @param string $name
+     */
+    public function setName($name = null)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get email of the user.
+     *
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set name for user.
+     *
+     * @param bool $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Set role id for user.
+     *
+     * @param bool $roleId
+     */
+    public function setRole($roleId)
+    {
+        $this->role_id = $roleId;
+    }
+
+    /**
+     * Get registration date of the user.
+     *
+     * @return mixed
+     */
+    public function getRegistrationDate()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Check if user has a role.
+     *
+     * @param string $role
+     *
+     * @return bool
+     */
+    public function hasRole($role = 'admin')
+    {
+        switch ($role) {
+            case 'admin':
+                return $this->isAdministrator();
+            case 'partner':
+                return $this->isClient();
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Check if user is administrator.
+     *
+     * @return bool
+     */
+    public function isAdministrator(): bool
+    {
+        return $this->role_id === Role::ADMIN_ROLE_ID;
+    }
+
+    /**
+     * Check if user is partner.
+     *
+     * @return bool
+     */
+    public function isClient(): bool
+    {
+        return $this->role_id === Role::CLIENT_ROLE_ID;
+    }
+
+    /**
+     * Get user's policy.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id');
     }
 }
