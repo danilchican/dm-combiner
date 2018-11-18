b1ad29b5b837d4514c01179a2a6ca8945ffeb55a
danilchican <danilchican@mail.ru>
2018-11-18 17:17:03 +0300

    Fixed users timestamps and role relation

--- database/migrations/2018_11_18_135602_add_role_relation_to_user.php
+++ database/migrations/2018_11_18_135602_add_role_relation_to_user.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -14,7 +15,7 @@ class AddRoleRelationToUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('role_id')->after('id');
+            $table->unsignedInteger('role_id')->after('id')->default(Role::CLIENT_ROLE_ID);
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
