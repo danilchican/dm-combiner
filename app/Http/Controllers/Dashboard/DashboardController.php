bf0da1f771038d2b11b11a429460db64b28f7b71
danilchican <danilchican@mail.ru>
2018-11-21 17:11:34 +0300

    Minor updates for layout

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
+    private const USERS_PER_PAGE = 10;
+
     /**
      * Show index page of the dashboard.
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \InvalidArgumentException
      */
     public function __invoke()
     {
-        return view('dashboard.index');
+        $users = User::paginate(self::USERS_PER_PAGE);
+        return view('dashboard.home.index')->with(compact('users'));
     }
 }
