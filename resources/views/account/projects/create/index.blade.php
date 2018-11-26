a18c9bd14d8a3ff49de1692b999ee9c0a1e3ecaa
danilchican <danilchican@mail.ru>
2018-11-26 23:29:17 +0300

    Created second step for the creating project

--- resources/views/account/projects/create/index.blade.php
+++ resources/views/account/projects/create/index.blade.php
@@ -33,7 +33,7 @@
                             <li>
                                 <a href="#step-2">
                                     <span class="step_no">2</span>
-                                    <span class="step_descr">Step 2<br/><small>Set algorithm</small></span>
+                                    <span class="step_descr">Step 2<br/><small>Select & configure algorithms</small></span>
                                 </a>
                             </li>
                             <li>
@@ -50,23 +50,7 @@
                             @include('account.projects.create.tabs.1_step')
                         </div>
                         <div id="step-2">
-                            <h2 class="StepTitle">Step 2 Content</h2>
-                            <p>
-                                do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                                aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                                officia deserunt mollit anim id est laborum.
-                            </p>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                                dolor
-                                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                                mollit anim id est laborum.
-                            </p>
+                            @include('account.projects.create.tabs.2_step')
                         </div>
                         <div id="step-3">
                             <h2 class="StepTitle">Step 3 Content</h2>
