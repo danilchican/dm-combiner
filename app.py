bb68a2d663091b95d95aba97784a80342416beea
Shakun Roman <roman.shakun@backend.expert>
2018-12-14 11:31:06 +0300

    [ADD] integration with calary

--- app.py
+++ app.py
@@ -1,27 +1,69 @@
 import os
+import time
 
+from celery import Celery
 from flask import Flask, request, jsonify, send_file
 
-from conf.config import PROJECT_ROOT, STATIC_FILES
+from conf.config import STATIC_FILES
+from frameworks.framework import Framework
+from frameworks.skl import SKL
 from handlers.data_handler import DataHandler
 from handlers.json_handler import JsonHandler
-from frameworks.skl import SKL
-from frameworks.framework import Framework
 from utils import helpers
-from utils.logger import logger
 from utils.decorators import view_exception
+from utils.logger import logger
 
 app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 
 @app.route('/frameworks', methods=['GET'])
 @view_exception
 def get_frameworks():
+    task = my_background_task.apply_async(args=[10, 20])
+    print(task.id, task.state)
+    print(task.get())
     result = []
     frameworks = Framework().get_subclasses()
     for framework_name, framework_instance in frameworks.items():
         result.append({"name": framework_name, 'methods': list(framework_instance().methods.keys())})
     return jsonify({'success': True, 'frameworks': result})
+
+
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    print(task_id)
+    task = my_background_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        # job did not start yet
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
 
 
 @app.route('/args/<string:framework_name>/<string:method_name>', methods=['GET'])
@@ -85,23 +127,6 @@ def upload_file():
     return jsonify({'success': True, 'path': file_path})
 
 
-# --- Algorithm views
-@app.route('/preview/<string:filename>', methods=['GET'])
-@view_exception
-def preview(filename):
-    data_handler = DataHandler(os.path.join(STATIC_FILES, filename))
-    if data_handler.filter_file_extension(filename):
-        if data_handler.is_path_exist():
-            data = data_handler.show_file_preview()
-            if data is None:
-                return jsonify({'success': False, 'error': "Error during proccessing."})
-            return jsonify({'success': True, 'result': data})
-        else:
-            return jsonify({'success': False, 'error': "No such path"})
-    else:
-        return jsonify({'success': False, 'error': "Bad file extension."})
-
-
 @app.route('/algorithm', methods=['POST'])
 # @view_exception
 def algorithm():
@@ -141,5 +166,11 @@ def algorithm():
     return jsonify({'success': True, 'result': data})
 
 
+@celery.task
+def my_background_task(arg1, arg2):
+    time.sleep(10)
+    return arg1 + arg2
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
