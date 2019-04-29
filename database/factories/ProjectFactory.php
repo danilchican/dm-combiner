790ca29d69b47d6a81a197428e57af2d064d0234
danilchican <danilchican@mail.ru>
2019-04-29 14:01:23 +0300

    Fixed factory.

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -9,7 +9,7 @@ $factory->define(\App\Models\Project::class, function (Faker $faker) {
         'scale'         => $faker->boolean,
         'data_url'      => $faker->url,
         'columns'       => json_encode($faker->randomElements([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 10, true)),
-        'configuration' => $faker->text,
+        'configuration' => 'a:1:{i:0;a:3:{s:4:"name";s:7:"k_means";s:9:"framework";s:3:"SKL";s:6:"params";a:2:{s:10:"n_clusters";i:2;s:4:"init";s:9:"k-means++";}}}',
         'result'        => $faker->realText(),
     ];
 });
